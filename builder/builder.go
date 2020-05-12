5300cc62e05447b0556aa19f421d1db089501fc5
Ben Toogood <bentoogood@gmail.com>
2020-05-12 15:00:53 +0100

    Build without dumb-init

--- builder/builder.go
+++ builder/builder.go
@@ -25,12 +25,6 @@ const (
 	dockerfile = `
 FROM golang:1.13 as builder
 
-# Install Dumb Init
-RUN git clone https://github.com/Yelp/dumb-init.git
-WORKDIR ./dumb-init
-RUN make build
-WORKDIR ..
-
 # Copy the service
 ARG service_dir
 COPY $service_dir service
@@ -44,8 +38,7 @@ RUN CGO_ENABLED=0 go build -ldflags="-w -s" -v -o app .
 # https://github.com/GoogleContainerTools/distroless
 FROM gcr.io/distroless/static
 COPY --from=builder /go/service/app /service
-COPY --from=builder /go/dumb-init/dumb-init /dumb-init
-ENTRYPOINT ["dumb-init", "./service"]
+ENTRYPOINT ["./service"]
 `
 )
 
